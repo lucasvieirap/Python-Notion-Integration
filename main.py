b15538c2e55a9333bc081d14b78f5ac9ca7f13d1
lucasvieirap <lucasvieirapalavro@gmail.com>
2025-10-12 07:36:15 -0300

    refactor: add build_row function

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import os, csv
 from notion_client import Client
 from dotenv import load_dotenv
 
+def build_row(num_columns, cells, row=[]): 
+    for index, cell in enumerate(cells):
+
+        try:
+            cell_content = cell[0]['text']['content']
+        except IndexError:
+            cell_content = ''
+
+        row.insert(len(row), cell_content)
+
+        if index >= num_columns:
+            continue
+
+    return row
+
 def main():
 
     load_dotenv()
@@ -14,42 +29,31 @@ def main():
     page_content = notion.blocks.children.list(block_id=str(page_id))
 
     table_objects = list(filter(lambda obj: obj['type'] == 'table', page_content['results']))
+    table_ids = list(map(lambda tab: tab['id'], table_objects))
 
-    for table_obj_index, obj in enumerate(table_objects):
+    for table_obj_index, table_id in enumerate(table_ids):
 
         with open(f"data{table_obj_index}.csv", 'w', newline='') as f:
             writer = csv.writer(f)
 
-            table_id = obj['id']
-            table_content = notion.blocks.children.list(block_id=table_id)
+            table_contents = notion.blocks.children.list(block_id=table_id)
             
-            for field in table_content['results']:
+            for table_content in table_contents['results']:
 
                 try:
-                    cells = field['table_row']['cells']
+                    cells = table_content['table_row']['cells']
                 except KeyError:
                     cells = ''
 
                 num_columns = len(cells)
 
-                row = []
-                for index, cell in enumerate(cells):
+                row = build_row(num_columns, cells)
 
-                    try:
-                        cell_content = cell[0]['text']['content']
-                    except IndexError:
-                        cell_content = ' '
+                writer.writerow(row)
 
-                    row.insert(len(row), cell_content)
-                    if index >= num_columns - 1:
-                        writer.writerow(row)
-                        row.clear()
-                    if index <= num_columns - 2:
-                        pass
+                row.clear()
 
         print(f"Wrote data{table_obj_index}.csv file")
-
-
 
 
 if __name__ == '__main__':
