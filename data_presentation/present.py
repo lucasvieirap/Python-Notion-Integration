52200137b80aa84b2cb9480383a1913434864ac2
lucasvieirap <lucasvieirapalavro@gmail.com>
2025-10-15 21:30:04 -0300

    feat: add line-chart creation

--- data_presentation/present.py
+++ data_presentation/present.py
@@ -1,30 +1,98 @@
-import os
+import os, csv, string, math
 import pandas as pd
 
-def build_table_from_obj(table_obj, title):
+import xlsxwriter
+from xlsxwriter.color import Color
+
+def create_csv_from_table_obj(data, filename):
+    with open(filename, 'w', newline='') as f:
+
+        writer = csv.writer(f)
+
+        table = build_table_from_obj(data, filename.split('.')[0])
+        writer.writerows(table)
+
+    print(f"Wrote file: {filename}")
+
+def create_linechart_from_data(data, filename) -> xlsxwriter.Workbook:
+
+    workbook = xlsxwriter.Workbook(f"{filename}.xlsx")
+    worksheet = workbook.add_worksheet()
+    number_format = workbook.add_format({'num_format': '#'})
+
+    chart = workbook.add_chart({'type': 'line'})
+    chart.set_title({'name': '=Sheet1!$A$1'})
+
+    max = 0
+
+    for row_index, row in enumerate(data):
+        for cell_index, cell in enumerate(row): 
+            try: 
+                worksheet.write_number(row_index, cell_index, float(cell), number_format)
+
+                if max < float(cell):
+                    max = math.floor(float(cell)+(float(cell)/3))
+
+            except (TypeError, ValueError):
+                worksheet.write(row_index, cell_index, cell)
+
+            if cell_index > 0 and row_index == 1: 
+
+                current_column = string.ascii_uppercase[cell_index]
+                last_row_index = len(data)
+
+                chart.add_series({
+                    'name': f'=Sheet1!${current_column}$2',
+                    'categories': f'=Sheet1!$A$3:$A${last_row_index}',
+                    'values': f'=Sheet1!${current_column}$3:${current_column}${last_row_index}',
+                    'marker': {'type': 'circle'}
+                })
+
+    chart.set_x_axis({
+        'name': 'Set',
+    })
+    chart.set_y_axis({
+        'name': 'Reps', 
+        'min': 0,
+        'max': max
+    })
+
+    chart.set_style(11)
+
+    worksheet.insert_chart('F2', chart)
+
+    return workbook
+
+def create_chart_from_csv(csvfilepath):
+
+    csv_data = []
+
+    with open(csvfilepath, newline='') as csvfile:
+
+        reader = csv.reader(csvfile)
+
+        for row in reader:
+            csv_data.append(row)
+
+    workbook = create_linechart_from_data(csv_data, 'data')
+    workbook.close()
+
+def build_table_from_obj(table_obj):
 
     table_row_objs = table_obj['results']
 
     table = []
     table_row = []
-    table_title = []
 
     for row in table_row_objs:
 
         try:
             cells = row['table_row']['cells']
         except KeyError:
-            cells = ''
+            cells = None
 
         table_row = build_row(cells)
         table.append(table_row)
-
-    table_title.append(title)
-
-    for _ in range(0, (len(table[0])-1)):
-        table_title.append('')
-
-    table.insert(0, table_title)
 
     return table
 
@@ -37,7 +105,7 @@ def build_row(cells):
         try:
             cell_content = cell[0]['text']['content']
         except IndexError:
-            cell_content = ''
+            cell_content = None
 
         row.append(cell_content)
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, sys, csv
+import os, sys
 from notion_client import Client
 from dotenv import load_dotenv
 
@@ -11,7 +11,7 @@ def main():
         print("Usage: \n\t python main.py [notion_url]\n")
         return
 
-    page_id = collect.get_pageid_from_url(sys.argv[1][-32:])
+    page_id = collect.get_pageid_from_url((sys.argv[1])[-32:])
 
     load_dotenv()
 
@@ -23,20 +23,12 @@ def main():
 
     table_objs = collect.get_table_objects(notion, page_content)
 
-    for table_obj_index, table_obj in enumerate(table_objs): 
-        filename = f"notion_table_{table_obj_index}.csv"
+    for table_obj in table_objs: 
+        table = present.build_table_from_obj(table_obj)
+        title = table[0][0]
 
-        with open(filename, 'w', newline='') as f:
-
-            writer = csv.writer(f)
-
-            table = present.build_table_from_obj(table_obj, filename.split('.')[0])
-
-            writer.writerows(table)
-
-        print(f"Wrote file: {filename}")
-
-    present.export_to_xlsx("./", "./notion_tables_sheets.xlsx")
+        workbook = present.create_linechart_from_data(table, title)
+        workbook.close()
 
 if __name__ == '__main__':
     main()
