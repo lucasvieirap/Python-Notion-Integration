e0e5c6df22acf1db433453eaa86c8020f2672e57
lucasvieirap <lucasvieirapalavro@gmail.com>
2025-10-22 18:49:21 -0300

    chore: change engine to xlsxwriter

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ from data_collection import collect
 from data_presentation import present
 
 import pandas as pd
-import openpyxl
-from openpyxl.chart import (
-    LineChart,
-    Reference,
-)
-from openpyxl.chart.axis import Scaling
 
 def main():
 
@@ -33,10 +27,7 @@ def main():
     notion = Client(auth=notion_secret)
 
     cell_pos = (0, 0)
-    chart_pos = (0, 0)
-
     table_objs = []
-
     filepath = "./tables_notion.xlsx"
 
     table_obj_dict = {}
@@ -50,9 +41,9 @@ def main():
     for table_obj in table_objs:
         table_obj_constructed = present.build_table_from_obj(table_obj)
 
-        try:
+        if table_obj_constructed[0][0] in table_obj_dict.keys():
             table_obj_dict[table_obj_constructed[0][0]] += table_obj_constructed[2:]
-        except KeyError:
+        else:
             table_obj_dict[table_obj_constructed[0][0]] = table_obj_constructed[1:]
 
         for obj_dict_index, obj_dict in enumerate(table_obj_dict[table_obj_constructed[0][0]][1:]):
@@ -65,22 +56,14 @@ def main():
             else:
                 obj_dict[0] = f'{obj_dict_index+1}th'
 
-    # print(table_obj_dict)
-
-    if not os.path.exists(filepath):
-        workbook = openpyxl.Workbook()
-        workbook.save(filepath)
-
     with pd.ExcelWriter(filepath, 
-                        engine="openpyxl", 
-                        if_sheet_exists="overlay",
-                        mode="a") as writer:
+                        engine="xlsxwriter", 
+                        mode="w") as writer:
 
         cell_pos = (cell_pos[0], 0)
 
         workbook = writer.book
         chart_worksheet = None
-
 
         for name, values in table_obj_dict.items():
             table_data = [[name], *values]
@@ -92,54 +75,65 @@ def main():
                               sheet_name="Sheet")
 
             values_list = []
-            for value in values[1:]:
-                values_list += value[1:]
+            series_list = []
+            for row in values[1:]:
+                values_list += row[1:]
+
+            series_headers = values[0]
+            series_count = len(values[0])-1
+
+            for i in range(1, len(values[0])):
+                series_list.append(list(map(lambda x: None if i >= len(x) else x[i], values[1:])))
 
             max_value = max(list(filter(lambda x: x != None, values_list)))
 
-            linechart = LineChart()
-
             chart_sheet_name = f"{name} CHART"
 
-            chart_worksheet = workbook.create_sheet(chart_sheet_name)
+            chart_worksheet = workbook.add_worksheet(chart_sheet_name)
 
-            (min_col, min_row) = (cell_pos[1] + 2, cell_pos[0]+3)
-            (max_col, max_row) = (cell_pos[1] + table_df.shape[1], cell_pos[0]+1 + table_df.shape[0])
+            linechart = workbook.add_chart({ 'type': 'line' })
+            linechart.set_title({ 'name': name })
+            linechart.set_style(13)
+            linechart.set_y_axis({
+                'name': 'Reps',
+                'min': 0,
+                'max': max_value+(max_value/3),
+            })
 
-            (categories_min_col, categories_min_row) = (cell_pos[1]+1, 
-                                                        cell_pos[0]+4)
-            (categories_max_col, categories_max_row) = (
-                cell_pos[1]+1, 
-                cell_pos[0]+1 + table_df.shape[0]
-            )
+            linechart.set_size({
+                'x_scale': 2.3,
+                'y_scale': 2.3,
+            })
 
-            linechart = LineChart()
-            linechart.title = name
-            linechart.style = 13
-            linechart.y_axis.delete = False
-            linechart.y_axis.title = "Reps"
-            linechart.y_axis.scaling = Scaling(min=0, max=max_value+(max_value/3))
+            for serie_index in range(0, series_count):
 
-            data = Reference(workbook['Sheet'], 
-                             min_col=min_col, 
-                             min_row=min_row,
-                             max_col=max_col,
-                             max_row=max_row)
+                (min_col, min_row) = (cell_pos[1] + (serie_index + 2), cell_pos[0]+4)
+                (max_col, max_row) = (cell_pos[1] + (serie_index + 2), cell_pos[0]+1 + table_df.shape[0])
 
-            categories = Reference(workbook['Sheet'], 
-                                   min_col=categories_min_col, 
-                                   min_row=categories_min_row,
-                                   max_col=categories_max_col,
-                                   max_row=categories_max_row)
+                (catmin_col, catmin_row) = (cell_pos[1]+1, 
+                                            cell_pos[0]+4)
+                (catmax_col, catmax_row) = (
+                    cell_pos[1]+1, 
+                    cell_pos[0]+1 + table_df.shape[0]
+                )
 
-            linechart.add_data(data, titles_from_data=True)
-            linechart.set_categories(categories)
+                min_col_letter = present.number_to_alphabet(min_col)
+                max_col_letter = present.number_to_alphabet(max_col)
 
-            chart_worksheet.add_chart(linechart, "A1")
+                catmin_col_letter = present.number_to_alphabet(catmin_col)
+                catmax_col_letter = present.number_to_alphabet(catmax_col)
+
+                linechart.add_series({
+                    'name': series_headers[serie_index+1],
+                    'values': f'=Sheet!${min_col_letter}${min_row}:'+
+                                      f'{max_col_letter}${max_row}',
+                    'categories': f'=Sheet!${catmin_col_letter}${catmin_row}:'+
+                                          f'{catmax_col_letter}${catmax_row}',
+                })
+
+            chart_worksheet.insert_chart('A1', linechart)
 
             cell_pos = (cell_pos[0], cell_pos[1] + len(values[0]))
-
-        workbook.save(filepath)
 
 if __name__ == '__main__':
     main()
